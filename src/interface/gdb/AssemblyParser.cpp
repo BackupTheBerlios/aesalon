1ea14de9c4df6f6860b12c4c9fc217d88d624a18
strange <kawk256@gmail.com>
2009-11-07 23:48:33 -0700

    Re-added platform support to the buildsystem.
    No platform auto-detection, though, I think I learned my lesson last time.
    Perhaps in the future.

--- src/interface/gdb/AssemblyParser.cpp
+++ src/interface/gdb/AssemblyParser.cpp
@@ -1,6 +1,10 @@
 #include "AssemblyParser.h"
 #include <iostream>
 
+#ifndef AESALON_PLATFORM
+    #error "AESALON_PLATFORM undefined"
+#endif
+
 namespace Aesalon {
 namespace Interface {
 namespace GDB {
@@ -13,10 +17,17 @@ AssemblyParser::~AssemblyParser() {
 
 }
 
+#if AESALON_PLATFORM == AESALON_PLATFORM_x86
+
+    #error "x86 support is currently NYI"
+
+#elif AESALON_PLATFORM == AESALON_PLATFORM_x86_64
 bool AssemblyParser::changes_memory(std::string asm_line) {
     std::cout << "AssemblyParser: asked if \"" << asm_line << "\" changes memory . . ." << std::endl;
+    
     return false;
 }
+#endif
 
 } // namespace GDB
 } // namespace Interface
