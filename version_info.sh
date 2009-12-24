9126dbb504a80e2fdff9619eb0f75696d3e0ee5a
strange <kawk256@gmail.com>
2009-12-24 14:55:59 -0800

    Added automated system for determing aesalon version, based off of git tags.
    Basically, the most recent tag with the name of "version_X.Y.Z" will become
    aesalon version X.Y.Z.
    
    Now, back to the ptrace system.

---
+++ version_info.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "# Automatically generated on " $(date) > version_info
+git tag -l 'version_*'|sort -r | head -n 1 | awk -F '_' '{print $2}' | awk -F '.' '{ print "set(AESALON_MAJOR_VERSION " $1 ")\nset(AESALON_MINOR_VERSION " $2 ")\nset(AESALON_PATCHLEVEL " $3 ")" }' >> version_info
+
