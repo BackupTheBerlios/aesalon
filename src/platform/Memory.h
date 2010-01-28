c7ad9d2c4820ee80c81892fdcd0d9a17a6bd5892
strange <kawk256@gmail.com>
2010-01-28 13:51:32 -0700

    Well, I've come to a conclusion . . .
    And that conclusion is: platform needs to go. It is no longer required . . .
    or, at least, most of it is no longer required. Taking a look at the source
    files in src/platform/, the only source files being used are: Event and
    EventQueue. Well, and ArgumentList. But ArgumentList can be put into misc, and
    so can Event and EventQueue. In essence, I can redefine the roles of each
    namespace: Monitor is all of the nitty-gritty stuff, the actual data
    collection, GUI is the fancy displaying-of-data, and Misc is anything that is
    common between the previous two. Platform is, so to speak, out.
    
    On a side note, with this commit, aesalon passes 6000 lines.

--- src/platform/Memory.h
+++ src/platform/Memory.h
@@ -12,6 +12,7 @@
 namespace Aesalon {
 namespace Platform {
 
+/** Stores the memory associated with a program. */
 class Memory {
 protected:
     typedef std::set<Misc::SmartPointer<MemoryBlock> > block_set_t;
@@ -23,11 +24,20 @@ private:
     }
     void remove_block(Misc::SmartPointer<MemoryBlock> block);
 public:
+    /** Generic constructor. */
     Memory() {}
+    /** Virtual destructor. */
     virtual ~Memory() {}
     
+    /** Returns the block at the specified MemoryAddress.
+        @param address The address to look for.
+        @return The @a MemoryBlock at @a address, or NULL if not found.
+    */
     Misc::SmartPointer<MemoryBlock> get_block(MemoryAddress address) const;
     
+    /** Handle an event as a "diff", and apply it to the current memory representation.
+        @param event The event to handle.
+    */
     void handle_event(Misc::SmartPointer<Event> event);
 };
 
--- src/platform/MemoryBlock.h
+++ src/platform/MemoryBlock.h
@@ -6,16 +6,35 @@
 namespace Aesalon {
 namespace Platform {
 
+/** A representation of a block of allocated memory. */
 class MemoryBlock {
 private:
+    /** The address of the block. */
     MemoryAddress address;
+    /** The size of the block. */
     MemoryAddress size;
 public:
+    /** Generic constructor.
+        @param address The address of the block.
+        @param size The size of the block.
+    */
     MemoryBlock(MemoryAddress address, MemoryAddress size) : address(address), size(size) {}
     
+    /** Gets the address of the current block.
+        @return The address of the block.
+    */
     MemoryAddress get_address() const { return address; }
+    /** Changes the address of the current block.
+        @param new_addres The new address.
+    */
     void set_address(MemoryAddress new_address) { address = new_address; }
+    /** Returns the current size of the current block.
+        @return The current size.
+    */
     MemoryAddress get_size() const { return size; }
+    /** Changes the size of the current block.
+        @param new_size The new size.
+    */
     void set_size(MemoryAddress new_size) { size = new_size; }
 };
 
