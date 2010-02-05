8b547830b7a53f21867d685ad467b95bcc1c28bd
strange <kawk256@gmail.com>
2010-02-05 12:15:08 -0700

    Mostly implemented a simple overload library using __malloc_hook.
    This means that the number of breakpoints used will be cut down dramatically,
    from two per allocation to only one, globally.
    
    So . . . next up, to actually gather that information, I suppose.

---
+++ monitor/src/overload/overload.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <malloc.h>
+
+void aesalon_initialize_overload();
+void *aesalon_malloc_hook(size_t size, const void *caller);
+void *aesalon_realloc_hook(void *ptr, size_t size, const void *caller);
+void aesalon_free_hook(void *ptr, const void *caller);
+
+void *(*aesalon_old_malloc_hook)(size_t size, const void *caller);
+void *(*aesalon_old_realloc_hook)(void *ptr, size_t size, const void *caller);
+void (*aesalon_old_free_hook)(void *ptr, const void *caller);
+void (*__malloc_initialize_hook)(void) = aesalon_initialize_overload;
+
+
+void aesalon_initialize_overload() {
+    aesalon_old_malloc_hook = __malloc_hook;
+    __malloc_hook = aesalon_malloc_hook;
+    aesalon_old_realloc_hook = __realloc_hook;
+    __realloc_hook = aesalon_realloc_hook;
+    aesalon_old_free_hook = __free_hook;
+    __free_hook = aesalon_free_hook;
+}
+
+void *aesalon_malloc_hook(size_t size, const void *caller) {
+    void *data;
+    __malloc_hook = aesalon_old_malloc_hook;
+    data = malloc(size);
+    __malloc_hook = aesalon_malloc_hook;
+    
+    /* Send off a SIGTRAP, this data is ready for collection . . . */
+    asm("int3");
+    
+    return data;
+}
+
+void *aesalon_realloc_hook(void *ptr, size_t size, const void *caller) {
+    void *data;
+    __realloc_hook = aesalon_old_realloc_hook;
+    data = realloc(ptr, size);
+    __realloc_hook = aesalon_realloc_hook;
+    
+    /* Send off a SIGTRAP, this data is ready for collection . . . */
+    asm("int3");
+    
+    return data;
+}
+
+void aesalon_free_hook(void *ptr, const void *caller) {
+    __realloc_hook = aesalon_old_realloc_hook;
+    free(ptr);
+    __realloc_hook = aesalon_realloc_hook;
+    
+    asm("int3");
+}
