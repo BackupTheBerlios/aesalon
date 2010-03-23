10c77bf73a84df76be99031d0bc707a408400b03
strange <kawk256@gmail.com>
2010-03-22 23:49:40 -0600

    I think I may have found a solution to the rendering speed.
    In essence, I have done a little thinking on the problem, and come up with
    what appears to be a feasible solution. The first step is to change
    VisualizationDisplay into something slightly more flexible. One of the more
    major issues is going to be caching the rendered images, but that's another
    problem. For a more detailed breakdown, I will list what will happen upon the
    user shifting the view of a visualization slightly, in this case, assume it is
    shifted upwards 20 pixels. The VisualizationDisplay immediately moves the
    content of the current QImage (or QPixmap, perhaps) downwards 20 pixels,
    resulting in an immediate change for the user. Then, the VDisplay asks the
    VRenderer to render a QImage of that 20 pixel strip, with the associated
    VRPoints provided as well.
    
    The VRenderer, running in its own thread, then finds the VData that intersect
    that region of the QImage, and tells them to re-render themselves. Once the
    QImage representing the updated region has been rendered, it is sent back to
    the VDisplay, where it is subsequently blitted onto the VRenderer's QImage.
    The temporary QImage is then destroyed (or perhaps cached if I get around to
    that bit). That eliminates the VCanvas as well, I believe.
    
    Well, it looks good in writing. How it looks in code is a completely different
    question . . .

--- tests/cpp_new_test.cpp
+++ tests/cpp_new_test.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
+class BaseClass {
+public:
+    BaseClass() {}
+    virtual ~BaseClass() {}
+
+    virtual int get_data() const = 0;
+};
+
+class DataClass : public BaseClass {
+private:
+    int data;
+public:
+    DataClass() : data(0) {}
+    virtual ~DataClass() {}
+
+    virtual int get_data() const { return data; }
+};
+
 int main(int argc, char *argv[]) {
-    char *data = NULL;
-    std::cout << "Allocating data, size 16 . . . " << std::endl;
-    data = new char[16];
+    BaseClass *data = NULL;
+    std::cout << "Allocating new DataClass . . . " << std::endl;
+    data = new DataClass();
     std::cout << "Allocated data, address is 0x" << std::hex << (unsigned long)data << " . . ." << std::endl;
     delete data;
     std::cout << "Deleted data, exiting . . ." << std::endl;
