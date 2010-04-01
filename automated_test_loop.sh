f91a961e7b6b46a5265aadc0a95542ebb12286cc
strange <kawk256@gmail.com>
2010-04-01 13:54:56 -0600

    First stable release of Aesalon.

--- automated_test_loop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while true; do
-    ./aesalon -w tests/automated_test && sleep 1
-done
-
--- version_info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "# Automatically generated on " $(date) > version_info
-echo "set(AESALON_MAJOR_VERSION 0)" >> version_info
-echo "set(AESALON_MINOR_VERSION 0)" >> version_info
-echo "set(AESALON_PATCHLEVEL 0)" >> version_info
-
-git describe --tags --abbrev=0 --match 'version_*' | awk -F '_' '{print $2}' | awk -F '.' '{ print "set(AESALON_MAJOR_VERSION " $1 ")\nset(AESALON_MINOR_VERSION " $2 ")\nset(AESALON_PATCHLEVEL " $3 ")" }' >> version_info
-
